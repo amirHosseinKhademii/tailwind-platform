a0825cb6bac241368539888cde677cd2328ffa9d
Amirhoosein khademi <amirhossein.khademi@rdsysco.com>
2021-04-18 19:29:23 +0430

    DTRG-428: Implement test for drop down option

--- src/components/drop-down-option/drop-down-option.test.tsx
+++ src/components/drop-down-option/drop-down-option.test.tsx
@@ -1,5 +1,5 @@
 import { DropDownOption } from ".";
-import { idFinder, classFinder, setUp, useTree } from "test";
+import { classFinder, setUp, slotFinder } from "test";
 
 describe("Drop down option", () => {
   let component;
@@ -7,26 +7,19 @@ describe("Drop down option", () => {
     () =>
       (component = setUp(DropDownOption, {
         active: true,
-        children: <p className="drop-down-option-test">test</p>,
+        children: <p className="children-class">test</p>,
       }))
   );
-  // it("SHOUD Match <DropDownOption/> TO SNAPSHOT", () => {
-  //   let dropDownOption = useTree(DropDownOption);
-  //   expect(dropDownOption).toMatchSnapshot();
-  // });
-  // it("SHOULD RENDER WRAPPER", () => {
-  //   const wrapper = idFinder(component, "drop-down-option");
-  //   expect(wrapper.length).toBe(1);
-  // });
-  // it("SHOULD RENDER CERTIAN CLASS WHEN IS ACTIVE", () => {
-  //   const wrapper = classFinder(component, ".text-indigo-500");
-  //   expect(wrapper.length).toBe(1);
-  // });
-  // it("SHOULD RENDER CHILDREN WITH CERTIAN CLASS", () => {
-  //   const wrapper = classFinder(component, ".drop-down-option-test");
-  //   expect(wrapper.length).toBe(1);
-  // });
-  it("test", () => {
-    expect(true).toBeTruthy();
+  it("SHOULD RENDER WRAPPER", () => {
+    const wrapper = slotFinder(component, "wrapper");
+    expect(wrapper.length).toBe(1);
+  });
+  it("SHOULD RENDER CERTIAN CLASS WHEN IS ACTIVE", () => {
+    const wrapper = classFinder(component, ".text-cyan-600");
+    expect(wrapper.length).toBe(1);
+  });
+  it("SHOULD RENDER CHILDREN WITH CERTIAN CLASS", () => {
+    const wrapper = classFinder(component, ".children-class");
+    expect(wrapper.length).toBe(1);
   });
 });
