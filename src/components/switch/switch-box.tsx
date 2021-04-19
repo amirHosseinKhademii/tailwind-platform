d29e7b1eae5dc403b6324fe663e09381b29f0d63
Amirhoosein khademi <amirhossein.khademi@rdsysco.com>
2021-04-19 12:15:47 +0430

    DTRG-428: FIx switch box issue

--- src/components/switch/switch-box.tsx
+++ src/components/switch/switch-box.tsx
@@ -1,7 +1,7 @@
 import { FC, memo } from "react";
 
 export const SwitchBox: FC<ISwitch> = memo(({ onChange, toggle, open }) => (
-  <div className="flex flex-row self-start items-center mt-2">
+  <div className="">
     <div
       className={`w-14 h-8 rounded-full  border flex flex-row items-center cursor-pointer px-1  ${
         open ? "bg-indigo-300 justify-end" : "bg-gray-300 justify-start"
