21edc97f9b7c760c7fa233410d0aefa9db5ae5ea
Amirhoosein khademi <amirhossein.khademi@rdsysco.com>
2021-04-27 21:19:41 +0430

    DTRG-440: Fix login skeleton issue

--- src/components/skeleton/index.tsx
+++ src/components/skeleton/index.tsx
@@ -4,7 +4,7 @@ export const Skeleton = () => {
   const { pathname } = useLocation();
   return (
     <div className="fixed inset-0 bg-gray-300">
-      {!pathname.includes("admin") && (
+      {!pathname.includes("admin") && !pathname.includes("login") && (
         <>
           <div
             className="fixed top-0 left-0 h-full flex flex-col  items-center  bg-blue-gray-700 w-20 animate-pulse"
