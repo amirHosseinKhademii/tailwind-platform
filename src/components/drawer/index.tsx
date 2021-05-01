4ecc3db3311dc2c5179802db54bd974c8ecfc964
AmirHossein Khademi <amirhossein.khademi@rdsysco.com>
2021-05-01 19:17:46 +0430

    DTRG-448: Fix drawer class

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -1,11 +1,13 @@
 import { FC, memo } from "react";
+import { classNames } from "utils";
 
 export const Drawer: FC<IDrawer> = memo(({ children, open }) => {
   return (
     <div
-      className={`fixed top-0 left-0 h-full flex  flex-col  shadow-2xl bg-blue-gray-800 transition ease-linear duration-200  z-30  ${
+      className={classNames(
+        "fixed top-0 left-0 h-full flex-col shadow-2xl trans z-30 bg-blue-gray-800 ",
         open ? " w-72" : "w-20 "
-      }`}
+      )}
       slot="wrapper"
     >
       {children && <div slot="children">{children}</div>}
