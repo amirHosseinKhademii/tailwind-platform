3f494e37f70e4fc7b1b30132c740ed64a7c06700
Amirhoosein khademi <amirhossein.khademi@rdsysco.com>
2021-05-01 22:35:44 +0430

    DTRG-448: Fix skeleton style

--- src/components/skeleton/index.tsx
+++ src/components/skeleton/index.tsx
@@ -1,18 +1,10 @@
-export const Skeleton = () => {
+const Skeleton = () => {
   return (
-    <div className="fixed inset-0 bg-gray-300">
-      <div
-        className="fixed top-0 left-0 h-full flex flex-col items-center  bg-blue-gray-700 w-20 animate-pulse"
-        slot="drawer"
-      >
-        <div className="w-14 h-14 mt-2 bg-gray-500 rounded-full" />
-        <div className="w-8 h-8 mt-4  bg-gray-500 rounded " />
-      </div>
-
-      <div className="w-full md:transform md:translate-x-20 md:w-[100%-5rem] py-16 px-10 flex flex-col ">
-        <div className="bg-gray-400 animate-pulse h-32 rounded" />
-        <div className="bg-gray-400 animate-pulse h-170 w-full mt-10 rounded" />
-      </div>
+    <div className="w-full  py-16 px-10 flex-col items-center">
+      <div className="w-full bg-cyan-200 animate-pulse h-[200px] rounded" />
+      <div className="w-full bg-cyan-200 animate-pulse h-[300px] rounded mt-10" />
+      <div className="w-full bg-cyan-200 animate-pulse h-40 rounded mt-10" />
     </div>
   );
 };
+export  {Skeleton};
